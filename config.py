d456d319915d851149c7450a3f3c225f51f67815
Trevor <supershadowplay@gmail.com>
2019-03-31 18:17:39 -0400

    Added config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+"""Config for StatusBot v0.1
+https://github.com/SuperShadowPlay/StatusBot
+"""
+
+# Your bot's token
+TOKEN = 'NDQ5MzUxOTU2ODEzNzA5MzEz.XJ6HqA.R9UOPnApBBoS0O2H1dyZnQBnPZE'
+
+# What role to detect
+statusDetect = discord.Status.null
+
+# The id of the role to give/take
+ROLEID = 'null'

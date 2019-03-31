8e976602fdb684b0057b983a6736a7ee7809c6c6
Trevor <supershadowplay@gmail.com>
2019-03-31 18:18:16 -0400

    Did a LOT of work

--- initBot.py
+++ initBot.py
@@ -1,9 +1,11 @@
-"""Invisible bot vAlpha."""
+"""Code to run for StatusBot v0.1
+https://github.com/SuperShadowPlay/StatusBot"""
 import discord
 import asyncio
 import time
-TOKEN = 'NDQ5MzUxOTU2ODEzNzA5MzEz.XJ6HqA.R9UOPnApBBoS0O2H1dyZnQBnPZE'
+from config import *
 preventPrematureUpdates = False
+#Role id (which role will be assigned to remove/give permissions)
 
 client = discord.Client()
 
@@ -27,19 +29,37 @@ async def printStatus():
 
 
 @client.event
+async def on_member_update(before, after):
+    """Scan members and see if they are offline/invisible or not."""
+    if after.status == discord.Status.offline:
+        await client.add_roles(after, discord.Object(id=ROLEID))
+    else:
+        await client.remove_roles(after, discord.Object(
+                                                    id=ROLEID))
+    asyncio.sleep(1)
+
+
+@client.event
 async def on_message(message):
-    """on_message portion, it's a big mixed bag."""
-    if message.content == '!status':
-        await client.send_message(message.channel, 'You are {0}'.format(
-                                message.author.status))
-    if message.content == '!members':
-        await client.send_message(message.channel,
-                                  'Members are: {0}'.format(
-                                   message.server.members))
-                                   #Gives dict that can be iterated like below
-    if message.content == '!memcon':
-        for x in message.server.members:
-            print('{0} is {1}'.format(x, x.status))
+    """When a message is detected by the bot, this function is activated.
+
+    This section consists of commands that can be sent via discord
+    and allows users to interact with the bot.
+    """
+    #Don't reply to self
+    if message.author == client.user:
+        return
+    if message.content == 'i/info':
+        await client.send_message(message.channel, '''This bot gives a role
+        when a certain status (online, offline, idle, dnd) is detected,
+        and removes that role when that the user no
+        longer has the specified status.
+        --------------------
+        This bot is also open-source, and the source can be found at
+        `https://github.com/SuperShadowPlay/StatusBot`
+        --------------------
+        (This bot has no other user commands
+        other than this one)''')
 
 
 @client.event
@@ -47,6 +67,7 @@ async def on_ready():
     """When bot is ready it will output a message and start afterwards."""
     global preventPrematureUpdates
     preventPrematureUpdates = True
+    await client.change_presence(game=discord.Game(name='i/help for commands'))
     print('Logged in as:')
     print(client.user.name)
     print(client.user.id)

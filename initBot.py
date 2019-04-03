0fd1db17f7e15e32fae2f02e16cc24c0bd4e10bf
Trevor <supershadowplay@gmail.com>
2019-04-03 17:52:05 -0400

    Finished adding customizability

--- initBot.py
+++ initBot.py
@@ -1,13 +1,41 @@
-"""Code to run for StatusBot v0.1
-https://github.com/SuperShadowPlay/StatusBot"""
+"""Code to run for StatusBot v0.1.
+
+https://github.com/SuperShadowPlay/StatusBot
+"""
 import discord
 import asyncio
 import time
-from config import *
-preventPrematureUpdates = False
-#Role id (which role will be assigned to remove/give permissions)
+import config
 
+#Global variables.
+preventPrematureUpdates = False
 client = discord.Client()
+#Rename config variables for easier use.
+TOKEN = config.TOKEN
+ROLEID = config.ROLEID
+#Find out which status to detect.
+if config.statusDetect.lower() == 'offline':
+    statusDetect = discord.Status.offline
+elif config.statusDetect.lower() == 'online':
+    statusDetect = discord.Status.online
+elif config.statusDetect.lower() == 'idle':
+    statusDetect = discord.Status.idle
+elif config.statusDetect.lower() == 'dnd':
+    statusDetect = discord.Status.dnd
+else:
+    print('''Valid value for \"statusDetect\" not found!
+Check README.md for instructions on valid statusDetect values.
+    \n''')
+    input('Press enter to quit...')
+    quit()
+#Check for other null configs
+if TOKEN == 'null' or ROLEID == 'null':
+    print(
+        '''Either your config file was not loaded correctly,
+or you did not configure it!
+    \n''')
+    input('Press enter to quit...')
+    quit()
 
 
 def getTime():
@@ -18,25 +46,25 @@ def getTime():
 
 @client.event
 async def printStatus():
-    """Print the status of the bot every two minutes."""
+    """Print the status of the bot every five minutes."""
     global preventPrematureUpdates
     await client.wait_until_ready()
     while not client.is_closed:
         if preventPrematureUpdates is True:
-            print('{0} still running at {1}'.format(client.user.name,
+            print('{0} still running || {1}'.format(client.user.name,
                   getTime()))
-        await asyncio.sleep(120)
+        await asyncio.sleep(300)
 
 
 @client.event
 async def on_member_update(before, after):
     """Scan members and see if they are offline/invisible or not."""
-    if after.status == discord.Status.offline:
+    if after.status == statusDetect:
         await client.add_roles(after, discord.Object(id=ROLEID))
     else:
         await client.remove_roles(after, discord.Object(
                                                     id=ROLEID))
-    asyncio.sleep(1)
+    asyncio.sleep(10)
 
 
 @client.event
@@ -49,7 +77,7 @@ async def on_message(message):
     #Don't reply to self
     if message.author == client.user:
         return
-    if message.content == 'i/info':
+    if message.content.startswith == 'i/info':
         await client.send_message(message.channel, '''This bot gives a role
         when a certain status (online, offline, idle, dnd) is detected,
         and removes that role when that the user no
@@ -68,9 +96,10 @@ async def on_ready():
     global preventPrematureUpdates
     preventPrematureUpdates = True
     await client.change_presence(game=discord.Game(name='i/help for commands'))
-    print('Logged in as:')
-    print(client.user.name)
-    print(client.user.id)
+    print('Logged in as {0}, ID {1} at {2}'.format(client.user.name,
+                                                   client.user.id, getTime()))
+    print('''Status detection set to {0}
+Role to give/take is ID {1}'''.format(statusDetect, ROLEID))
     print('------')
 
 client.loop.create_task(printStatus())

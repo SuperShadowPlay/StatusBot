2462fea2c5514304b19b2d249a414fad076a84b8
SuperShadowPlay <supershadowplay@gmail.com>
2019-04-06 11:02:52 -0400

    Changed i/help to correctly i/info

--- initBot.py
+++ initBot.py
@@ -95,7 +95,7 @@ async def on_ready():
     """When bot is ready it will output a message and start afterwards."""
     global preventPrematureUpdates
     preventPrematureUpdates = True
-    await client.change_presence(game=discord.Game(name='i/help for commands'))
+    await client.change_presence(game=discord.Game(name='i/info for commands'))
     print('Logged in as {0}, ID {1} at {2}'.format(client.user.name,
                                                    client.user.id, getTime()))
     print('''Status detection set to {0}
